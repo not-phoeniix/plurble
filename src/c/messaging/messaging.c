5f88e5861a5fd11a450a646cb35b17e56abae26b
Nikki Murello <nmurello15@gmail.com>
2025-10-02 14:09:10 -0400

    Reorganized messaging, implemented infinite data sending

--- src/c/messaging/messaging.c
+++ src/c/messaging/messaging.c
@@ -7,11 +7,7 @@
 //!   later via the compiler.... these will look like errors
 //!   in vscode but there's nothing wrong with em dw <3
 
-static void inbox_recieved_handler(DictionaryIterator* iter, void* context) {
-    ClaySettings* settings = settings_get();
-
-    // ~~~ app settings ~~~
-
+static void handle_settings_inbox(DictionaryIterator* iter, ClaySettings* settings) {
     Tuple* accent_color = dict_find(iter, MESSAGE_KEY_AccentColor);
     if (accent_color != NULL) {
         settings->accent_color = GColorFromHEX(accent_color->value->int32);
@@ -50,28 +46,85 @@ static void inbox_recieved_handler(DictionaryIterator* iter, void* context) {
             false
         );
     }
+}
 
-    // ~~~ API data ~~~
-
-    Tuple* members = dict_find(iter, MESSAGE_KEY_Members);
-    if (members != NULL) {
-        // members_set_members(members->value->cstring);
-    }
-
-    Tuple* custom_fronts = dict_find(iter, MESSAGE_KEY_CustomFronts);
-    if (custom_fronts != NULL) {
-        // members_set_custom_fronts(custom_fronts->value->cstring);
-    }
-
-    Tuple* fronters = dict_find(iter, MESSAGE_KEY_Fronters);
-    if (fronters != NULL) {
-        // members_set_fronters(fronters->value->cstring);
-    }
-
+static void handle_api_inbox(DictionaryIterator* iter, ClaySettings* settings) {
     Tuple* api_key_valid = dict_find(iter, MESSAGE_KEY_ApiKeyValid);
     if (api_key_valid != NULL) {
         settings->api_key_valid = api_key_valid->value->int16;
     }
+
+    static int frontable_counter = 0;
+    static int total_frontables = 0;
+    Tuple* num_total_frontables = dict_find(iter, MESSAGE_KEY_NumTotalFrontables);
+    if (num_total_frontables != NULL) {
+        APP_LOG(APP_LOG_LEVEL_INFO, "Identified start of frontable message sequence, clearing cached frontables!");
+
+        cache_clear_frontables();
+
+        // start the counting process!!!!!!!!
+        total_frontables = num_total_frontables->value->int32;
+        frontable_counter = 0;
+    }
+
+    Tuple* frontable_hash = dict_find(iter, MESSAGE_KEY_FrontableHash);
+    Tuple* frontable_name = dict_find(iter, MESSAGE_KEY_FrontableName);
+    Tuple* frontable_color = dict_find(iter, MESSAGE_KEY_FrontableColor);
+    Tuple* frontable_pronouns = dict_find(iter, MESSAGE_KEY_FrontablePronouns);
+    Tuple* frontable_is_custom = dict_find(iter, MESSAGE_KEY_FrontableIsCustom);
+
+    if (
+        frontable_hash != NULL &&
+        frontable_name != NULL &&
+        frontable_color != NULL &&
+        frontable_pronouns != NULL &&
+        frontable_is_custom != NULL
+    ) {
+        Frontable* f = frontable_create(
+            frontable_hash->value->uint32,
+            frontable_name->value->cstring,
+            frontable_pronouns->value->cstring,
+            frontable_is_custom->value->int16,
+            GColorFromHEX(frontable_color->value->int32)
+        );
+
+        cache_add_frontable(f);
+
+        frontable_counter++;
+    }
+
+    if (frontable_counter >= num_total_frontables) {
+        APP_LOG(APP_LOG_LEVEL_INFO, "All frontables recieved!");
+    }
+
+    static int current_front_counter = 0;
+    static int total_current_fronters = 0;
+    Tuple* num_current_fronters = dict_find(iter, MESSAGE_KEY_NumCurrentFronters);
+    if (num_current_fronters != NULL) {
+        APP_LOG(APP_LOG_LEVEL_INFO, "Identified start of current fronter message sequence, clearing cached current fronters!");
+
+        cache_clear_current_fronters();
+
+        // start the counting process :]
+        total_current_fronters = num_current_fronters->value->int32;
+        current_front_counter = 0;
+    }
+
+    Tuple* current_fronter = dict_find(iter, MESSAGE_KEY_CurrentFronter);
+    if (current_fronter != NULL) {
+        cache_add_current_fronter(current_fronter->value->uint32);
+    }
+
+    if (current_front_counter >= total_current_fronters) {
+        APP_LOG(APP_LOG_LEVEL_INFO, "All current fronters recieved!");
+    }
+}
+
+static void inbox_recieved_handler(DictionaryIterator* iter, void* context) {
+    ClaySettings* settings = settings_get();
+
+    handle_settings_inbox(iter, settings);
+    handle_api_inbox(iter, settings);
 
     settings_save();
 }
@@ -81,7 +134,7 @@ static void inbox_dropped_callback(AppMessageResult reason, void* context) {
 }
 
 static void outbox_sent_handler(DictionaryIterator* iter, void* context) {
-    printf("outbox sent !!");
+    APP_LOG(APP_LOG_LEVEL_INFO, "outbox sent!");
 }
 
 static void outbox_failed_callback(DictionaryIterator* iter, AppMessageResult reason, void* context) {
